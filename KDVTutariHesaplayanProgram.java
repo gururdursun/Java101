b304b2693146a1a9d2fa5410fd2bf982cb3bb4d7
gururdursun <111658761+gururdursun@users.noreply.github.com>
2022-09-03 16:42:56 +0300

    Temel Kavramlar ve Değişkenler

--- KDVTutariHesaplayanProgram.java
+++ KDVTutariHesaplayanProgram.java
@@ -8,7 +8,6 @@ KDV tutarı = 1.8;
 Ödev
 Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
 tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız. */
-
 import java.util.Scanner;
 public class KDVTutariHesaplayanProgram {
     public static void main(String[] args) {
