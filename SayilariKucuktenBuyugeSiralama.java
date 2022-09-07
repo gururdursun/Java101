e8d5a6eb0a74caa5d393586d3c47802e330d0805
gururdursun <111658761+gururdursun@users.noreply.github.com>
2022-09-07 15:45:12 +0300

    Koşullu İfadeler ve Kod Blokları

---
+++ SayilariKucuktenBuyugeSiralama.java
@@ -0,0 +1,36 @@
+package KosulluIfadelerveKodBloklari;
+import java.util.Scanner;
+public class SayilariKucuktenBuyugeSiralama {
+    public static void main(String[] args) {
+        int a,b,c;
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("1.Sayıyı giriniz: ");
+        a= input.nextInt();
+        System.out.print("2.Sayıyı giriniz: ");
+        b= input.nextInt();
+        System.out.print("3.Sayıyı giriniz: ");
+        c= input.nextInt();
+
+        if(a<b && a<c){
+            if(b<c){
+                System.out.println("Sıralama: " +(a)+"<"+(b)+"<"+(c));
+            }else{
+                System.out.println("Sıralama: " +(a)+"<"+(c)+"<"+(b));
+            }
+        }else if (b<a && b<c) {
+            if(a<c){
+                System.out.println("Sıralama: " +(b)+"<"+(a)+"<"+(c) );
+            }else{
+                System.out.println("Sıralama: " +(b)+"<"+(c)+"<"+(a));
+            }
+        }else{
+            if(b<a){
+                System.out.println("Sıralama: " +(c)+"<"+(b)+"<"+(a) );
+            }else {
+                System.out.println("Sıralama: " +(c)+"<"+(a)+"<"+(b));
+            }
+        }
+    }
+}
