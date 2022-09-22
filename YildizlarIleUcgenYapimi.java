4b4e0c572d6b8b71c56b1f7c72c3fe10707c7c7c
gururdursun <111658761+gururdursun@users.noreply.github.com>
2022-09-22 15:22:15 +0300

    Döngüler

---
+++ YildizlarIleUcgenYapimi.java
@@ -0,0 +1,33 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class YildizlarIleUcgenYapimi {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        n = input.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j <=  i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * n - (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
